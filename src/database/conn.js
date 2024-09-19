ad8458d3895d61d2193df1ca6278db29a4e5bdb9
leachenski <jleachenski@gmail.com>
2024-09-19 10:23:48 -0300

    feat: add env

--- src/database/conn.js
+++ src/database/conn.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const main = async () => {
   try {
-    await mongoose.connect(`${process.env.MONGODB}${process.env.DATABASE}`);
+    await mongoose.connect(`${process.env.MONGODB || "mongodb://localhost:27017/"}${process.env.DATABASE}`);
     console.log("Conectado com sucesso");
   } catch (error) {
     console.log(error);
