97a7949c2bbcc554ce6ddea9e1668ddba531d922
Vincent <vincecake008@gmail.com>
2024-09-22 16:04:29 -0300

    feat: dice main by vincent

--- src/main.js
+++ src/main.js
@@ -3,12 +3,15 @@ import e from "express";
 import user_router from "./http/routes/user-route.js";
 import roullete_bet_router from "./http/routes/roullete-bet-route.js";
 import jackpot_bet_router from "./http/routes/jackpot-bet-route.js";
+import dice_bet_router from "./http/routes/dice-bet-route-vincent.js";
+
 const app = e();
 
 app.use(e.json());
 app.use("/user", user_router);
 app.use("/roullete-bet", roullete_bet_router);
-app.use("/jackpot-bet", jackpot_bet_router)
+app.use("/jackpot-bet", jackpot_bet_router);
+app.use("/dice-bet", dice_bet_router);
 
 app.listen(process.env.API_PORT, () => {
   console.log("Aplicação rodando");
