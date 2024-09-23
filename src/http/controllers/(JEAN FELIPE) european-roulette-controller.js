7e17ba105f40d44d5de6ee329d99ae9482e2af82
Jean <JFBimCopinski@gmail.com>
2024-09-23 08:49:17 -0300

    feat: european roulette bet

---
+++ src/http/controllers/(JEAN FELIPE) european-roulette-controller.js
@@ -0,0 +1,62 @@
+import EuropeanRoulette from "../../database/models/(JEAN FELIPE) european-roulette-model.js"
+
+const store = async (req, res) => {
+    try {
+        //INÍCIO DA GERAÇÃO DE CORES
+        const collor = Math.ceil(Math.random() * 3)
+        /*
+        GERENCIAMENTO DE CORES;
+            1: COR PRETA;
+            2: COR VERMELHA;
+            3: COR BRANCA
+        */
+       //FIM DA GERAÇÃO DE CORES
+
+       //INÍCIO DA GERAÇÃO DE NÚMERO DA ROLETA
+       const roulette = Math.ceil(Math.random() * 36)
+       //FIM DA GERAÇÃO DE NNÚMERO DA ROLETA
+
+       attempt.Collor == collor && attempt.Roulette == roulette
+       ? (req.body.roulette.status = "YOU WON")
+       : (req.body.roulette.status = "YOU LOSE")
+
+       await EuropeanRoulette.create(req.body)
+       res.json()
+    } catch (err) {
+        res.status(400).json(err)
+    }
+}
+
+const index = async (req, res) => {
+    try {
+        let content = await EuropeanRoulette.find(req.query).exec()
+        res.json(content)
+    } catch (err) {
+        res.status(400).json(err)
+    }
+}
+
+const show = async (req, res) => {
+    try {
+        let content = await EuropeanRoulette.findById(req.params.query).exec()
+        res.json(content)
+    } catch (err) {
+        res.status(400).json(err)
+    }
+}
+
+const update = async (req, res) => {
+    try {
+        let content = await EuropeanRoulette.findByIdAndUpdate(req.params.query).exec()
+        res.json(content)
+    } catch (err) {
+        res.status(400).json(err)
+    }
+}
+
+export default {
+    store,
+    index,
+    show,
+    update
+}
