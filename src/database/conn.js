5212aea44b835ceedd4c2a22d24af611bdd03d90
Jhonatan Leachenski <130028211+jleachenski@users.noreply.github.com>
2024-09-23 09:14:36 -0300

    Update conn.js

--- src/database/conn.js
+++ src/database/conn.js
@@ -3,9 +3,7 @@ import mongoose from "mongoose";
 const main = async () => {
   try {
     await mongoose.connect(
-      `${process.env.MONGODB || "mongodb://localhost:27017/"}${
-        process.env.DATABASE
-      }`
+      `${process.env.MONGODB || "mongodb://localhost:27017/"}${process.env.DATABASE}`
     );
     console.log("Conectado com sucesso");
   } catch (error) {
